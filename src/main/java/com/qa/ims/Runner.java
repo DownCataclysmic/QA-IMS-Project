b2cc0b89f411d59fcbe1b395e1dc5d2ab2c9d002
Danny's Laptop <dannymaksoud@gmail.com>
2022-07-22 18:28:56 +0100

    k

--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -10,7 +10,7 @@ public class Runner {
 	public static void main(String[] args) {
 		IMS ims = new IMS();
 		ims.imsSystem();
-		LOGGER.info("Powering Down.");
+		LOGGER.info("SO LONg!");
 	}
-}
 
+}
